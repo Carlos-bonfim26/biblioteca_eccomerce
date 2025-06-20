a22e10df4881c469f9c8bcfed7061b73da38cb7e
carlosbonfim <carlosbonfim722@gmail.com>
2025-06-19 21:51:47 -0300

    feat: interface home e redirecionamento de link da navbar

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 require_once "src/conexao.php";
 
 session_start();
 
-if(!isset($_SESSION['Usuario'])){
+if (!isset($_SESSION['Usuario'])) {
     header('Location: pages/login.php');
     exit;
 }
@@ -11,18 +11,58 @@ if(!isset($_SESSION['Usuario'])){
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="author" content="Carlos Bonfim">
     <title>Compiloteca</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="shortcut icon" href="img/logoIcon.png" type="image/x-icon">
 </head>
+
 <body>
-    
-<h1>Olá mundo</h1>
-   
+    <div class="shape">
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-dark">
+                <div class="container-fluid ">
+        
+                    <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                      <ul class="nav ">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="index.php">Livros</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="pages/carrinho.php">Carrinho</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="pages/perfil.php">Perfil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="pages/contato.php">Fale Conosco</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <section class="home">
+            <img src="img/logoLight.png" alt="">
+            <h2>Feita por devs, para devs</h2>
+        
+        </section>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
+
 </html>
--- pages/perfil.php
+++ pages/perfil.php
@@ -37,7 +37,7 @@ $cliente = $result->fetch_assoc();
 
                     <ul class="nav justify-content-end">
                         <li class="nav-item">
-                            <a class="nav-link " aria-current="page" href="#">Home</a>
+                            <a class="nav-link " aria-current="page" href="../index.php">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="../index.php">Livros</a>
